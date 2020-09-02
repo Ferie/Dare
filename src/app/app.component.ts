9bdf43130735aab5ae1b9b609ca3b53772c2085d
Riccardo Andreatta <ferie150@hotmail.com>
2020-09-02 20:22:00 +0100

    Passing the navigation array to the Navigation component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { NavItem } from './interfaces/navigation-item.interface';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+    public navigationArray: Array<NavItem>;
+
+    /**
+     * Pass the navigation array to the component.
+     */
+    constructor() {
+        this.navigationArray = [
+            {
+                index: 1,
+                menuLabel: 'Blusher',
+                link: '/blusher'
+            },
+            {
+                index: 2,
+                menuLabel: 'Brushes',
+                link: '/brushes'
+            },
+            {
+                index: 3,
+                menuLabel: 'Mascara',
+                link: '/mascara'
+            },
+            {
+                index: 4,
+                menuLabel: 'Perfume',
+                link: '/perfume'
+            },
+            {
+                index: 5,
+                menuLabel: 'Hair',
+                link: '/hair'
+            }
+        ];
+    }
 }
