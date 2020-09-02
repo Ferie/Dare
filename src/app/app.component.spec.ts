bb9ca9c3320e70f31a631453da77a85a4ff51624
Riccardo Andreatta <ferie150@hotmail.com>
2020-09-02 20:21:23 +0100

    App component created

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,28 @@
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AppComponent],
+            imports: [RouterTestingModule]
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        // Create component and test fixture
+        fixture = TestBed.createComponent(AppComponent);
+        // Instatiate test component from the fixture
+        component = fixture.componentInstance;
+        // Detect the changes in the component
+        fixture.detectChanges();
+    });
+
+    it('should create the app', () => {
+        expect(component).toBeTruthy();
+    });
+});
